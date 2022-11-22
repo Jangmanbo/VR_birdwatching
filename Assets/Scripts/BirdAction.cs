2721a94879d21999b22a70c3d9b21cc2117c90dd
Jangmanbo <wkddpdnjs99@gmail.com>
2022-11-23 01:54:15 +0900

    update : BirdAction
    - 항상 앞을 보며 이동
    - 랜덤으로 이동 속도 및 회전 방향 결정
    - 이동 속도가 일정 속도 이하면 이동 및 회전 중지

--- Assets/Scripts/BirdAction.cs
+++ Assets/Scripts/BirdAction.cs
@@ -6,23 +6,46 @@ public class BirdAction : MonoBehaviour
 {
     private Transform tr;
     private Rigidbody rb;
-    private float moveSpeed = 5;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float turnSpeed;
+
+    public Vector3 moveDir;
+    public Vector3 turnDir;
 
     private void Awake()
     {
-        tr = gameObject.GetComponent<Transform>();
-        rb = gameObject.GetComponent<Rigidbody>();
+        tr = GetComponent<Transform>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(MoveDirCoroutine());
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    private void Update()
     {
-        rb.velocity=new Vector3(1, 0, 1);
+        tr.Translate(moveDir * Time.deltaTime);
+        tr.Rotate(turnDir * Time.deltaTime);
+    }
+
+    IEnumerator MoveDirCoroutine()
+    {
+        while (true)
+        {
+            System.Random rand = new System.Random();
+
+            moveDir = Vector3.forward * (float)rand.NextDouble() * moveSpeed;
+
+            turnDir = new Vector3((float)rand.NextDouble() * 90 - 45, (float)rand.NextDouble() * 180 - 90, (float)rand.NextDouble() * 90 - 45);
+            turnDir = turnDir * (float)rand.NextDouble() * turnSpeed;
+
+            // Á¤Áö
+            if (moveDir.z < 1)
+                moveDir = turnDir = Vector3.zero;
+
+            yield return new WaitForSeconds(3f);
+        }
     }
 }
