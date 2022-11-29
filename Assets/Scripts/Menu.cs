af2dd8a6e9a9626f42f1278ac7502c9f998a1544
Jangmanbo <wkddpdnjs99@gmail.com>
2022-11-29 23:45:03 +0900

    add : Guide interaction

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,12 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
-    public void ClickBtn()
+    private List<Button> guideButtons = new List<Button>();
+    private List<TextMeshProUGUI> guideText = new List<TextMeshProUGUI>();
+
+    [SerializeField] private GameObject Guide;
+
+    private void Awake()
     {
-        Debug.Log("버튼 클릭");
+        guideButtons = Guide.GetComponentsInChildren<Button>().ToList();
+        guideText = Guide.GetComponentsInChildren<TextMeshProUGUI>().ToList();
+
+        for (int i = 0; i < guideButtons.Count; i++)
+        {
+            int id = i;
+            guideButtons[i].onClick.AddListener(()=>ClickBtn(id));
+        }
+    }
+    public void ClickBtn(int id)
+    {
+        Debug.Log(id+"번째 버튼 클릭");
     }
 }
  
